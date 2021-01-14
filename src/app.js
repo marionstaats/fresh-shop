4f66fa7cbf67c3241aec481ddcdc9a13b08c795e
marionstaats <70648703+marionstaats@users.noreply.github.com>
2021-01-14 12:11:32 +0100

    adding search bar heroku improv

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 const passport = require("passport");
 const path = require('path');
 const dotenv = require("dotenv");
-const bodyParser = require("body-parser");
 const LocalStrategy = require("passport-local"); 
 const User = require("./models/user");
 
